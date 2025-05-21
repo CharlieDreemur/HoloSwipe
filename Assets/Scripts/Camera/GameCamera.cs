b824a62fc113aa841de380ba6f4f881a97acd7c6
Ethan Wang <70038315+ewang223@users.noreply.github.com>
2025-05-21 15:18:04 -0700

    Scuffed, but time paradox shader now applies when camera is in the sphere
    There are some angles where its really sudden, but hopefully it feels fine

--- Assets/Scripts/Camera/GameCamera.cs
+++ Assets/Scripts/Camera/GameCamera.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class GameCamera : MonoBehaviour
 {
     [SerializeField] Transform playerloc;
+    [SerializeField] GameObject ParadoxFilter; 
+
     public float minX, maxX, minZ, maxZ;
     public float angle = 60; // yeah nah just gotta change both values i aint dealing with quaternions 
     public float height;
@@ -14,4 +16,48 @@ public class GameCamera : MonoBehaviour
         Mathf.Clamp(transform.position.x, minX, maxX);
         Mathf.Clamp(transform.position.z, minZ, maxZ);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TimeParadox"))
+        {
+            //ParadoxFilter.SetActive(true);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("TimeParadox"))
+        {
+            ParadoxFilter.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("TimeParadox"))
+        {
+            ParadoxFilter.SetActive(false);
+        }
+    }
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("TimeParadox"))
+        {
+            ParadoxFilter.SetActive(true);
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("TimeParadox"))
+        {
+            ParadoxFilter.SetActive(true);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("TimeParadox"))
+        {
+            //ParadoxFilter.SetActive(false);
+        }
+    }*/
 }
