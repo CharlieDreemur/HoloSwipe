03a275ff3c8a53464d1564066464de9e3445e3ff
CharlieDreemur <zhangwj.charlie@gmail.com>
2025-05-31 02:37:23 +0800

    create special item + fix official scene

--- Assets/BoothManager.cs
+++ Assets/BoothManager.cs
@@ -4,15 +4,15 @@ public class BoothManager : MonoBehaviour
 {
     [SerializeField] MerchBehaviour[] merchBehaviours;
 
-     MerchSO[] merchPool;
+    MerchSO[] merchPool;
     [SerializeField] MerchCollcetion merchPoolCollction;
     private void Start()
     {
 
         if(merchPoolCollction != null)
             merchPool = merchPoolCollction.merches;
-
-        if(GameManager.instance != null)
+        
+        else if(GameManager.instance != null)
             merchPool = GameManager.instance.merchPool.merches;
         StockShop();
     }
