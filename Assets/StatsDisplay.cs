3bbaf35528b1fe739f050ed100ff489eb38769f4
Ethan Wang <70038315+ewang223@users.noreply.github.com>
2025-05-29 18:24:08 -0700

    shouldve fixed statdisplay speed/discount error

--- Assets/StatsDisplay.cs
+++ Assets/StatsDisplay.cs
@@ -14,9 +14,9 @@ public class StatsDisplay : MonoBehaviour
     void Update()
     {
         fanScore.text = "" + (int)PlayerStatManager.fanScore;
-        speedMult.text = "+" + (int)(PlayerStatManager.speedMultiplier-1)*100 + "%";
+        speedMult.text = "+" + (int)((PlayerStatManager.speedMultiplier-1)*100) + "%";
         salary.text = "" + (int)PlayerStatManager.salary + "";
-        discount.text = "-" + (int)PlayerStatManager.discount*100 + "%";
+        discount.text = "-" + (int)(PlayerStatManager.discount*100) + "%";
         luck.text = "" + (int)PlayerStatManager.luck + "";
         contime.text = "+" + (int)PlayerStatManager.conTime + "s";
     }
